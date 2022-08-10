1f5f13732e3ead75ace5e7eb7a3e1f9c13af3276
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-08-10 16:25:59 +0300

    some changes

--- lesson_4_nodemon-master/src/app.ts
+++ lesson_4_nodemon-master/src/app.ts
@@ -1,7 +1,35 @@
-import express from 'express';
+import 'reflect-metadata';
+
+import express, { Request, Response } from 'express';
+import { createConnection, getManager } from 'typeorm';
+import { User } from './entity/user';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(5500, () => {
+app.get(
+    '/users',
+    async (req: Request, res: Response) => {
+        const users = await getManager().getRepository(User).find();
+        res.json(users);
+    },
+);
+
+app.post('/users', async (req: Request, res: Response) => {
+    console.log(req.body);
+    // const createdUser = await getManager().getRepository(User).save(req.body);
+    // res.json(createdUser);
+});
+
+app.listen(5500, async () => {
     console.log('Server started on port 5500!!!');
+    try {
+        const connection = await createConnection();
+        if (connection) {
+            console.log('Database Connected');
+        }
+    } catch (err) {
+        console.log(err);
+    }
 });
