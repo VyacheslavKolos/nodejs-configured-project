215b848d709e8e50c07745d7a1cf69228db8e088
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-08-26 11:09:01 +0300

    com

---
+++ lesson_4_nodemon-master/src/migration/1659786629470-createTablePosts.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createTablePosts1659786629470 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'posts',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'title',
+                    type: 'varchar',
+                    width: 250,
+                    isUnique: true,
+                    isNullable: false,
+                },
+                {
+                    name: 'text',
+                    type: 'varchar',
+                    width: 250,
+                    isNullable: false,
+                },
+                {
+                    name: 'userId',
+                    type: 'int',
+                },
+                {
+                    name: 'createdAt',
+                    type: 'timestamp',
+                    isNullable: false,
+                    default: 'now()',
+                },
+                {
+                    name: 'deleteAt',
+                    type: 'timestamp',
+                    isNullable: true,
+                },
+            ],
+
+            foreignKeys: [
+                {
+                    columnNames: ['userId'],
+                    referencedTableName: 'users',
+                    referencedColumnNames: ['id'],
+                    onDelete: 'CASCADE',
+                    onUpdate: 'CASCADE',
+                },
+            ],
+        }), true);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('posts', true);
+    }
+}
