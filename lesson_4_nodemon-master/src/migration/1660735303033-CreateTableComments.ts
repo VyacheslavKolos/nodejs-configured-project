840335f5773521fcd243713a0b4f5b18501cb523
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-08-26 11:13:13 +0300

    com

---
+++ lesson_4_nodemon-master/src/migration/1660735303033-CreateTableComments.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableComments1660735303033 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'comments',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'text',
+                    type: 'varchar',
+                    width: 250,
+                    isNullable: false,
+                },
+                {
+                    name: 'postId',
+                    type: 'int',
+                },
+                {
+                    name: 'likes',
+                    type: 'int',
+                },
+                {
+                    name: 'createdAt',
+                    type: 'timestamp',
+                    isNullable: false,
+                    default: 'now()',
+                },
+                {
+                    name: 'deleteAt',
+                    type: 'timestamp',
+                    isNullable: true,
+                },
+            ],
+
+            foreignKeys: [
+                {
+                    columnNames: ['postId'],
+                    referencedTableName: 'posts',
+                    referencedColumnNames: ['id'],
+                    onDelete: 'CASCADE',
+                    onUpdate: 'CASCADE',
+                },
+            ],
+        }), true);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('comments', true);
+    }
+}
